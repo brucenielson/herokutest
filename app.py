71979197c8df999a75fd40536a753dbb5edaea72
brucenielson <brucenielson1@gmail.com>
2015-02-14 13:16:54 -0700

    Part 2

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
+#app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import *
 
 
 @app.route('/')
@@ -13,4 +20,3 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
-
